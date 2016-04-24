57b218ca883ec034776fb9fdd6eb1c722978c069
scard69 <vit.scard@gmail.com>
2016-04-24 15:37:46 -0300

    atualização prontuario

---
+++ prontuario_app/js/medicacoes-controller.js
@@ -0,0 +1,8 @@
+// adicionado o AppModule para 'herdar' a lista de clientes
+angular.module('MedicacoesModule', ['AppModule']).
+        
+        controller('MedicacoesController', ['$scope', function ($scope) {
+                
+ 
+}]);
+
